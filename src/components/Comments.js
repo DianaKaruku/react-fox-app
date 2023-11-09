6c33204a61577eff057b08c8e105c355eee45325
Diana Karuku <diana.karuku@student.moringaschool.com>
2023-11-09 18:18:41 +0300

    created comments functions

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,1 +1,40 @@
+import React, { useState } from 'react';
 
+function Comments() {
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState('');
+
+  const handleCommentChange = (e) => {
+    
+    setNewComment(e.target.value);
+  };
+
+  const handleAddComment = () => {
+   
+    setComments(prevComments => [...prevComments, newComment]);
+
+    
+    setNewComment('');
+  };
+
+  return (
+    <div>
+      <h1>Comments</h1>
+      <div>
+        <textarea
+          placeholder="Add your comment..."
+          value={newComment}
+          onChange={handleCommentChange}
+        ></textarea>
+        <button onClick={handleAddComment}>Add Comment</button>
+      </div>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Comments;
